131ac6a2af60763e76e83df2708ca9d8331bec7a
tfenne <tfenne@tfenne.com>
2016-09-20 13:21:50 -0400

    Added a task for TrimPrimers.

---
+++ tasks/src/main/scala/dagr/tasks/fgbio/TrimPrimers.scala
@@ -0,0 +1,48 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 Fulcrum Genomics LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package dagr.tasks.fgbio
+
+import dagr.commons.CommonsDef._
+import htsjdk.samtools.SAMFileHeader.SortOrder
+
+import scala.collection.mutable.ListBuffer
+
+class TrimPrimers(val in: PathToBam,
+                  val out: PathToBam,
+                  val primers: FilePath,
+                  val slop: Option[Int] = None,
+                  val hardClip: Option[Boolean] = None,
+                  val sortOrder: Option[SortOrder] = None
+                 ) extends FgBioTask {
+
+  override protected def addFgBioArgs(buffer: ListBuffer[Any]): Unit = {
+    buffer.append("-i", in)
+    buffer.append("-o", out)
+    buffer.append("-p", primers)
+    slop.foreach(s => buffer.append("-S", s))
+    hardClip.foreach(h => buffer.append("-H", h))
+    sortOrder.foreach(so => buffer.append("-s", so))
+  }
+}
