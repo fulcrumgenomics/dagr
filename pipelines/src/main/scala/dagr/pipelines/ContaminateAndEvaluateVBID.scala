d828e56b7aa54b38f6751a8de6ed0f098c4d08e5
Yossi Farjoun <farjoun@gmail.com>
2020-12-03 23:37:04 -0500

    WIP. nearly there. need to hook up vcf for VBID and figure out how to appropriately downsample it using the targets.

--- pipelines/src/main/scala/dagr/pipelines/ContaminateAndEvaluateVBID.scala
+++ pipelines/src/main/scala/dagr/pipelines/ContaminateAndEvaluateVBID.scala
@@ -25,21 +25,19 @@ package dagr.pipelines
 
 import java.nio.file.{Files, Path}
 
-import dagr.commons.io.Io
+//import dagr.commons.io.Io
 import dagr.core.cmdline.Pipelines
-import dagr.core.config.Configuration
 import dagr.core.execsystem.{Cores, Memory}
-import dagr.core.tasksystem.{EitherTask, Linker, NoOpInJvmTask, NoOpTask, Pipeline, SimpleInJvmTask}
+import dagr.core.tasksystem.{EitherTask, Pipeline, SimpleInJvmTask}
 import dagr.sopt.{arg, clp}
-import dagr.tasks.DagrDef.{DirPath, FilePath, PathToBam, PathToFasta, PathToFastq, PathToIntervals}
-import dagr.tasks.bwa.BwaMem
-import dagr.tasks.misc.{LinkFile, MoveFile}
-import dagr.tasks.picard.{CollectHsMetrics, CollectMultipleMetrics, DeleteBam, DownsampleSam, DownsamplingStrategy, MarkDuplicates, SortSam}
+import dagr.tasks.DagrDef._
+import dagr.tasks.misc.LinkFile
+import dagr.tasks.picard.{CollectHsMetrics, DownsampleSam, DownsamplingStrategy, MergeSamFiles}
+//import htsjdk.samtools.metrics.MetricsFile
+//import _root_.picard.analysis.directed.HsMetrics
 import htsjdk.samtools.SAMFileHeader.SortOrder
-import htsjdk.samtools.metrics.MetricsFile
-import picard.analysis.CollectMultipleMetrics.Program
-import picard.analysis.directed.HsMetrics
 
+//import scala.collection.JavaConversions._
 import scala.collection.mutable
 
 /**
@@ -68,27 +66,22 @@ class ContaminateAndEvaluateVBID
     val tmpBam = out.resolve(prefix + ".tmp.bam")
     val metricsPrefix: Some[DirPath] = Some(out.resolve(prefix))
     Files.createDirectories(out)
-    val bamYield = new mutable.HashMap[PathToBam, Integer]
-
-    // Collect the median target coverage in each of the inputs and put result in a map
-    val yieldsCollected = new NoOpTask()
-    root ==> yieldsCollected
+    val bamYield = new mutable.HashMap[PathToBam, Int]
 
     bams.map(bam => {
-      val prefixPath: Path = out.resolve(prefix + bam.getFileName)
       val metricPath: Path = out.resolve(prefix + bam.getFileName + ".qualityYieldMetrics")
-      val hsMetrics = new CollectHsMetrics(in = bam, ref = ref, targets = targets)
+      val hsMetrics = new CollectHsMetrics(in = bam, ref = ref, targets = targets, prefix = Some(out.resolve(prefix)))
 
-      val collectYield = new CollectMultipleMetrics(in = bam, prefix = Some(prefixPath),
-        ref = ref, programs = Seq(Program.CollectQualityYieldMetrics))
       val fetchMedian = new FetchMedianCoverage(metricPath)
-      root ==> hsMetrics ==> fetchMedian ==> SimpleInJvmTask(bamYield.put(bam, fetchMedian.medianCoverage.getOrElse(0))) ==> yieldsCollected
+      root ==> hsMetrics ==> fetchMedian ==> SimpleInJvmTask(bamYield.put(bam, fetchMedian.medianCoverage.getOrElse(0)))
     })
 
     val pairsOfBam = for (x <- bams; y <- bams) yield (x, y)
 
     pairsOfBam.foreach { case (x, y) => {
       contaminations.foreach(c => {
+        val dsX = out.resolve(prefix + x.getFileName + ".forCont." + c + ".tmp.bam")
+        val dsY = out.resolve(prefix + y.getFileName + ".forCont." + c + ".tmp.bam")
         // create a mixture of c x + (1-c) y taking into account the depths of
         // x and y
         // D_x and D_y are the effective depths of x and y
@@ -100,44 +93,58 @@ class ContaminateAndEvaluateVBID
 
         val copyX = new LinkFile(x, dsX)
         val copyY = new LinkFile(y, dsY)
+        val downsampleX_Q = c / (1 - c) <= 1
+        val downsample = EitherTask.of(
+          downsampleX :: copyY :: Nil,
+          downsampleY :: copyX :: Nil,
+          downsampleX_Q)
 
-        val downsample = EitherTask.of(downsampleX, downsampleY, c / (1 - c) <= 1) ::
-          EitherTask.of(copyY, copyX, c / (1 - c) <= 1)
+        val xFactor = if (downsampleX_Q) c / (1 - c) else 1
+        val yFactor = if (downsampleX_Q) 1 else (1 - c) / c
 
+        val resultantDepth: Double = bamYield.getOrElse(x, 0) * xFactor + bamYield.getOrElse(y, 0) * yFactor
+
+        val xyContaminatedBam = out.resolve(prefix + "__" + x.getFileName + "__" + y.getFileName +
+          "__" + c + ".bam")
+        val mergedownsampled = new MergeSamFiles(in = dsX :: dsY :: Nil, out = xyContaminatedBam, sortOrder = SortOrder.coordinate)
+
+        root ==> downsample ==> mergedownsampled
+
+        depths filter (d => d <= resultantDepth) foreach { d => {
+          val outDownsampled = out.resolve(prefix + "__" + x.getFileName + "__" + y.getFileName +
+            "__" + c + "__target__" + d + ".bam")
+          val downsampleMerged = new DownsampleSam(in = xyContaminatedBam, out = outDownsampled, proportion = d / resultantDepth)
+
+          mergedownsampled ==> downsampleMerged
+          markers.foreach { markerCount => {}
+
+            // subset VBID resource files to smaller number of markers
+
+            // call VBID using subsetted markers
+
+          }
+
+        }
+        }
       })
+
     }
     }
-
-    depths.foreach(depth => {
-      con
-
-
-    })
-    val bwa = new BwaMem(fastq = fastq, ref = ref)
-    val sort = new SortSam(in = Io.StdIn, out = tmpBam, sortOrder = SortOrder.coordinate) with Configuration {
-      requires(Cores(2), Memory("2G"))
-    }
-    val mark = new MarkDuplicates(inputs = Seq(tmpBam), out = Some(bam), prefix = metricsPrefix) with Configuration {
-      requires(Cores(1), Memory("2G"))
-    }
-    val rmtmp = new DeleteBam(tmpBam)
-
-    root ==> (bwa | sort) ==> mark ==> rmtmp
-    targets.foreach(path => root ==> new CollectHsMetrics(in = bam, prefix = metricsPrefix, targets = path, ref = ref))
-  }
-
-}
-
-/** Pops open the HS metrics file and reads out the median coverage. */
-private class FetchMedianCoverage(hsMetrics: FilePath) extends SimpleInJvmTask {
-  var medianCoverage: Option[Int] = None
-
-  override def run(): Unit = {
-    val mfile = new MetricsFile[HsMetrics, java.lang.Integer]
-    mfile.read(Io.toReader(hsMetrics))
-    val median = mfile.getMetrics.map(hs => hs.MEDIAN_TARGET_COVERAGE).max
-    this.medianCoverage = Some(median.toInt)
   }
 }
+//
+///** Pops open the HS metrics file and reads out the median coverage. */
+//private class FetchMedianCoverage(hsMetrics: FilePath) extends SimpleInJvmTask {
+//  var medianCoverage: Option[Int] = None
+//
+//  override def run(): Unit = {
+//    val mfile = new MetricsFile[HsMetrics, java.lang.Integer]
+//    mfile.read(Io.toReader(hsMetrics))
+//    val median = mfile.getMetrics.map(hs => hs.MEDIAN_TARGET_COVERAGE).max
+//    this.medianCoverage = Some(median.toInt)
+//  }
+//}
+
+
 
 
